8d0417d02f12a90b10ba6885aeeaf9eba8b5daa0
Christian Panter <pantercp@outlook.com>
2023-11-01 08:17:47 +0100

    Condensed code

--- js/global.js
+++ js/global.js
@@ -11,41 +11,30 @@ const darkModeToggle = document.querySelector('#dark-mode-toggle');
 const activateMoon = () => {
 
   if (darkModeToggle.classList.contains("day")) {
-    console.log("CONTAINS DAY")
     darkModeToggle.classList.remove("day")
-    console.log("REMOVED DAY")
     moonIcon.classList.remove("sun")
-    console.log("REMOVED SUN")
   }
   else {
-    console.log("DOESN'T CONTAIN DAY")
   }
 }
 
 const activateSun = () => {
 
   if (darkModeToggle.classList.contains("day")) {
-    console.log("ALREADY CONTAINS DAY")
   }
   else {
     darkModeToggle.classList.add("day")
-    console.log("DAY ADDED")
     moonIcon.classList.add("sun")
-    console.log("SUN ADDED")
   }
 }
-
 
 const enableDarkMode = () => {
   // 1. Add the class to the body
   document.body.classList.add('darkmode');
-
   // 2. Add moon to toggle switch
   activateMoon();
-
   LogoIm.src = "images/shams-dark-notext.png"
   LogoIm2.src = "images/shams-dark-notext.png"
-
   // 3. Update darkMode in localStorage
   localStorage.setItem('darkMode', 'enabled');
 }
@@ -53,17 +42,10 @@ const enableDarkMode = () => {
 const disableDarkMode = () => {
   // 1. Remove the class from the body
   document.body.classList.remove('darkmode');
-
   // 2. Add Sun to toggle switch
   activateSun();
   LogoIm.src = "images/shams-light-notext.png"
   LogoIm2.src = "images/shams-light-notext.png"
-
-
-  darkModeToggle.classList.add("day")
-  moonIcon.classList.add("sun")
-
-
   // 3. Update darkMode in localStorage 
   localStorage.setItem('darkMode', null);
 }
@@ -76,7 +58,6 @@ if (darkMode === 'enabled') {
 else{
   disableDarkMode()
 }
-
 
 // When someone clicks the button
 darkModeToggle.addEventListener('click', () => {
@@ -91,20 +72,6 @@ darkModeToggle.addEventListener('click', () => {
     disableDarkMode();
   }
 });
-
-// -----------New Toggle--------
-
-
-// $('.tdnn').click(function () {
-//   // $("body").toggleClass('light');
-//   $(".moon").toggleClass('sun');
-//   $(".tdnn").toggleClass('day');
-// });
-
-//   <!--See version 2.0 withoug jQuery-->
-
-// darkModeToggle.classList.toggle(".day")
-
 
 // -----------Header--------
 
